2fd9a143817d192f05e94e98e8638fef7c331f64
mustahsanK <108030556+mustahsanK@users.noreply.github.com>
2025-06-08 17:38:56 +0000

    Update ContentController.java

--- src/main/java/com/funcexec/content_calendar/controller/ContentController.java
+++ src/main/java/com/funcexec/content_calendar/controller/ContentController.java
@@ -1,11 +1,18 @@
 package com.funcexec.content_calendar.controller;
 
+import com.funcexec.content_calendar.repository.ContentCollectionRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ContentController {
 
+    private final ContentCollectionRepository repository;
+
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
+    
     @GetMapping("/")
     public String content() {
         return "Content Calendar";

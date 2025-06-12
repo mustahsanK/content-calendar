1d9c04e03e77bb2dc94a0f570d8660ad2d8d6d78
mustahsanK <108030556+mustahsanK@users.noreply.github.com>
2025-06-12 17:51:33 +0000

    Update ContentCollectionRepository.java

--- src/main/java/com/funcexec/content_calendar/repository/ContentCollectionRepository.java
+++ src/main/java/com/funcexec/content_calendar/repository/ContentCollectionRepository.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ContentCollectionRepository {
 	private final List<Content> content = new ArrayList<>();
 
